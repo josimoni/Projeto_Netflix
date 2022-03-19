4d0baef9df36b555fa419abeb80ffd18b5d34671
josimoni <josimoni@live.com>
2022-03-19 10:29:26 -0300

    Add functions on bottons and posters
    Função no botão "saiba mais"
    Link no cartaz
    Add mais um filme

--- js/Owl/setup.js
+++ js/Owl/setup.js
@@ -16,6 +16,11 @@ $('.owl-carousel').owlCarousel({
 
    
 })
+// funções dos botões do filme principal
+
+function botao1() {
+    window.open("https://filmow.com/o-reino-dos-gatos-t10273/")
+}
 
 function botao2() {
     window.open("https://youtu.be/Gp-H_YOcYTM")
@@ -23,4 +28,31 @@ function botao2() {
 
 function botao3() {
     window.open("https://studioghibli.com.br/filmografia/o-reino-dos-gatos/")
+}
+
+
+// funções dos posters
+
+function garfield1() {
+    window.open("https://youtu.be/GV5y4yTDtBI")
+}
+
+function aristogatos() {
+    window.open("https://youtu.be/NsaQpBmHF6E")
+}
+
+function garfield2() {
+    window.open("https://youtu.be/wNqDjaOHFaw")
+}
+
+function felix() {
+    window.open("https://youtu.be/1ScTWJkcsrw")
+}
+
+function mandachuva() {
+    window.open("https://youtu.be/_xGIxD-hrxA")
+}
+
+function hellokitty() {
+    window.open("https://youtu.be/rkq3M66Po_c")
 }
